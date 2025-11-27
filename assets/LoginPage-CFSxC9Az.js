35aa169eb300f34713fa157d5e87dbd72768e3ab
Your Name <you@example.com>
2025-11-28 00:50:27 +0800

    Updates

---
+++ assets/LoginPage-CFSxC9Az.js
@@ -0,0 +1,1 @@
+import{f as t,r as a}from"./index-ego2Aw0s.js";const o=()=>{const e=t();return a.useEffect(()=>{e("/?auth=login",{replace:!0})},[e]),null};export{o as default};
---
+++ assets/RegisterPage-E7q8ZjxD.js
@@ -0,0 +1,1 @@
+import{f as t,r}from"./index-ego2Aw0s.js";const s=()=>{const e=t();return r.useEffect(()=>{e("/?auth=register",{replace:!0})},[e]),null};export{s as default};
