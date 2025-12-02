e50ed29523eaa1ee8f46cd230e3b8882982a4c82
Your Name <you@example.com>
2025-12-03 00:18:59 +0800

    Updates

--- assets/LoginPage-BOmqU_Ro.js
+++
@@ -1,1 +0,0 @@
-import{f as t,r as a}from"./index-hoivcvTL.js";const o=()=>{const e=t();return a.useEffect(()=>{e("/?auth=login",{replace:!0})},[e]),null};export{o as default};
---
+++ assets/RegisterPage-DQ4787b0.js
@@ -0,0 +1,1 @@
+import{f as t,r}from"./index-Ymay8exD.js";const s=()=>{const e=t();return r.useEffect(()=>{e("/?auth=register",{replace:!0})},[e]),null};export{s as default};
