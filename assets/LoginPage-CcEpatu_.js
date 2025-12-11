7d35343a10d747181c6202c90619567c7671af65
Your Name <you@example.com>
2025-12-11 22:12:25 +0800

    Updates

---
+++ assets/LoginPage-CcEpatu_.js
@@ -0,0 +1,1 @@
+import{u as t,r as a}from"./index-QiRdDBcQ.js";const o=()=>{const e=t();return a.useEffect(()=>{e("/?auth=login",{replace:!0})},[e]),null};export{o as default};
--- assets/LoginPage-Xs1ifDtG.js
+++
@@ -1,1 +0,0 @@
-import{u as t,r as a}from"./index-DZ_vDZZm.js";const o=()=>{const e=t();return a.useEffect(()=>{e("/?auth=login",{replace:!0})},[e]),null};export{o as default};
